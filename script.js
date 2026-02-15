e3a9d60d0aa02f243be3fb90a597eecc54f1fc69
Chris-Solutions-MX <chris.solutions.mx.oficial@gmail.com>
2026-02-15 17:41:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function contactar(){
-alert("WhatsApp: +52 (993) 103 3859 (Chris Solutions)");
-window.open("https://wa.me/5219931033859", "_blank");
+  alert("WhatsApp: +52 (993) 103 3859 (Chris Solutions)");
+  window.open("https://wa.me/5219931033859", "_blank");
 }
+
+// PRELOADER
+window.addEventListener("load", function(){
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  preloader.style.visibility = "hidden";
+});
